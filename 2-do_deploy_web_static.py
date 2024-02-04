7da0353b30f74e85a545294ed39faa1b0bdfcaa8
MinaSamirSaad <minasamir.nashid@gmail.com>
2024-02-04 23:47:49 +0200

    fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,23 +11,34 @@ env.hosts = ['34.224.1.205', '107.23.61.147']
 
 
 def do_deploy(archive_path):
-    '''copies archive file from local to my web servers'''
+    """
+    copies archive file from local to my web servers
+    """
+
     if not path.exists(archive_path):
         return False
     try:
         file_name = archive_path.split("/")[-1].split(".")[0]
         put(archive_path, "/tmp/")
+
         run("mkdir -p /data/web_static/releases/{}".format(file_name))
-        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}"
+
+        run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
             .format(file_name, file_name))
-        run("rm /tmp/{}.tgz".format(file_name))
-        run(("mv /data/web_static/releases/{}/web_static/*" +
-            "/data/web_static/releases/{}/")
+
+        run('rm -rf /tmp/{}.tgz'.format(file_name))
+
+        run(('mv /data/web_static/releases/{}/web_static/* ' +
+            '/data/web_static/releases/{}/')
             .format(file_name, file_name))
-        run("rm -rf /data/web_static/releases/{}/web_static"
+
+        run('rm -rf /data/web_static/releases/{}/web_static'
             .format(file_name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{} /data/web_static/current"
+
+        run('rm -rf /data/web_static/current')
+
+        run(('ln -s /data/web_static/releases/{}/' +
+            ' /data/web_static/current')
             .format(file_name))
         return True
     except Exception:
